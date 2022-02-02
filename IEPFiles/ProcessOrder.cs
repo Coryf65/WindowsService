7cf5bbf146493adfea376a2bb2532c488c392260
Cory <coryf65@gmail.com>
2022-02-02 14:01:56 -0600

    Logging to errors file and all daily rolling

--- IEPFiles/ProcessOrder.cs
+++ IEPFiles/ProcessOrder.cs
@@ -18,7 +18,7 @@ namespace IEPFiles
             _logger.LogInformation($"Starting job id {jobId}");
 
             await Task.Delay(3000); // wait 3 seconds
-
+            _logger.LogWarning("some Error happened");
             _logger.LogInformation($"job with id: {jobId} is completed.");
 
             // return Task.FromResult(true);
--- IEPFiles/Program.cs
+++ IEPFiles/Program.cs
@@ -5,9 +5,19 @@ using Serilog;
 // creating the Serilog ILogger
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
+    .WriteTo.File(path: "logs/logs.txt",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                rollingInterval: RollingInterval.Day) // setting file log levels to the file
+    .WriteTo.File(path: "logs/errors.txt",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning) // will only write Errors to this file
     .CreateLogger();
 
-IHost host = Host.CreateDefaultBuilder(args)
+
+try
+{
+    Log.Information("Starting service...");
+
+    IHost host = Host.CreateDefaultBuilder(args)
     .UseSerilog()
     .ConfigureServices(services =>
     {
@@ -17,13 +27,27 @@ IHost host = Host.CreateDefaultBuilder(args)
     })
     .Build();
 
-host.Services.UseScheduler(schedule =>
-{
-    var jobSchedule = schedule.Schedule<ProcessOrder>();
-    // when and how our job will run
-    // jobSchedule.EveryFiveMinutes().Weekday(); // example
-    
-    jobSchedule.EverySeconds(2).PreventOverlapping("ProcessOrderJob");
-});
+    host.Services.UseScheduler(schedule =>
+    {
+        var jobSchedule = schedule.Schedule<ProcessOrder>();
+        // when and how our job will run
+        // jobSchedule.EveryFiveMinutes().Weekday(); // example
 
-await host.RunAsync();
+        jobSchedule.EverySeconds(2).PreventOverlapping("ProcessOrderJob");
+    });
+
+    await host.RunAsync();
+}
+catch (Exception error)
+{
+    // log any errors while setting up the host
+    Log.Fatal(error, "Exception while setting up Host and scheduler");
+}finally
+{
+    // cleanup
+    Log.Information("Exiting service...");
+    Log.CloseAndFlush();
+}
+
+
+
