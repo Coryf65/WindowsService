8758ea5591ff5131bcb50f004a2e56cce6041cca
Cory <coryf65@gmail.com>
2022-02-03 14:00:49 -0600

    adding comments

--- IEPFiles/OrderQueueConnector.cs
+++ IEPFiles/OrderQueueConnector.cs
@@ -6,6 +6,7 @@ namespace IEPFiles
     {
         private readonly ILogger<OrderQueueConnector> _logger;
         private readonly QueueClient _orderQueueClient;
+        private readonly QueueClient _errorQueue;
 
         public OrderQueueConnector(ILogger<OrderQueueConnector> logger)
         {
@@ -13,8 +14,13 @@ namespace IEPFiles
             var connectionString = Environment.GetEnvironmentVariable("STORAGE_CONNECTION"); // using .env file
             var queueOptions = new QueueClientOptions { MessageEncoding = QueueMessageEncoding.Base64 };
 
+            // our orders queue we want to process
             _orderQueueClient = new QueueClient(connectionString, "customer-orders", queueOptions);
             _orderQueueClient.CreateIfNotExists();
+
+            // the bad or errors that occur in processing our other queue
+            _errorQueue = new QueueClient(connectionString, "error-customer-orders", queueOptions);
+            _errorQueue.CreateIfNotExists();
         }
 
         public async Task<OrderInfo> GetNextOrder()
@@ -24,13 +30,24 @@ namespace IEPFiles
 
             if (response.Value is not null)
             {
-                var order = response.Value.Body.ToObjectFromJson<OrderInfo>();
-                order.QueueMessageId = response.Value.MessageId;
-                order.QueuePopReciept = response.Value.PopReceipt;
+                try
+                {
+                    var order = response.Value.Body.ToObjectFromJson<OrderInfo>();
+                    order.QueueMessageId = response.Value.MessageId;
+                    order.QueuePopReciept = response.Value.PopReceipt;
 
-                return order;
+                    return order;
+                }
+                catch (Exception error)
+                {
+                    _logger.LogError(error, "Error in deserializing the message", response.Value);
+
+                    // put into error queue
+                    await _errorQueue.SendMessageAsync(response.Value.Body);
+                    // remove from order queue
+                    await _orderQueueClient.DeleteMessageAsync(response.Value.MessageId, response.Value.PopReceipt);
+                }                
             }
-
             return null;
         }
 
