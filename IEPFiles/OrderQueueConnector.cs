ea8d17dd73098ebe6a1949a4eedf4a955a5c0f5a
Cory <coryf65@gmail.com>
2022-02-03 13:11:40 -0600

    adding a class

--- IEPFiles/OrderQueueConnector.cs
+++ IEPFiles/OrderQueueConnector.cs
@@ -2,6 +2,8 @@
 {
     public class OrderQueueConnector : IOrderConnector
     {
+        private readonly ILogger<OrderQueueConnector> logger;
+
         public Task<OrderInfo> GetNextOrder()
         {
             throw new NotImplementedException();
